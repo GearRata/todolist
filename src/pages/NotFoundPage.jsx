28239983d1bd7706b74baec92b486d07f183aa44
Gear <gearmcc2545@gmail.com>
2025-07-11 22:36:41 +0700

    feat: add text

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,7 +2,7 @@ const NotFoundPage = () => {
   return (
     <div className="text-center">
       <h1 className="text-gray-300 font-bold text-7xl">404 ERROR</h1>
-      <h2 className="mt-6 font-bold text-3xl">Page Not Found</h2>
+      <h2 className="mt-6 font-bold text-3xl">Page Not Found So sad</h2>
       <p className="text-xl">The page you are looking for does not exits.</p>
     </div>
   );
